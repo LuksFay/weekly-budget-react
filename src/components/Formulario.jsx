a16dea315d1b0807af57405f5d262cbb1f8a0ac8
luksfay <lukspicco@hotmail.com>
2021-09-09 12:18:39 -0300

    shortid usage

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,31 @@
 import React, {useState} from 'react';
+import Error from "./Error";
+const shortid = require('shortid');
 
 const Formulario = () => {
 
     const [nombre, guardarNombre]  = useState("");
     const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
     
     //Cuando el usuario agrega un gasto
     const agregarGasto = e => { e.preventDefault ();
     
     //Validar
-
+        if(cantidad < 1 || isNaN(cantidad) || nombre.trim() === ""){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
     //Construir el gasto
-
+    // npm i shortid
+    // documentacion: https://www.npmjs.com/package/shortid
+    const gasto = {
+        nombre,
+        cantidad,
+        id: shortid.generate()
+    }
+    console.log(gasto)
     //Pasar el gasto al componente principal
 
     //Reiniciar el Form
@@ -23,7 +37,8 @@ const Formulario = () => {
     return (
         <form onSubmit={agregarGasto}>
             <h2>Agrega tus gastos aquí</h2>
-
+            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto incorrecto" /> : null }
+            
             <div className="campo">
                 <label> Nombre Gasto</label>
                 <input type="text" className="u-full-width" placeholder="Ej. Transporte"
